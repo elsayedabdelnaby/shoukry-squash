374abcbe532f64d1283f6cbb63f33280470155fa
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2024-06-03 13:28:02 +0300

    fix issue

--- resources/views/sections/teams.blade.php
+++ resources/views/sections/teams.blade.php
@@ -24,34 +24,35 @@
                             <div class="social-style-two">
                                 @if ($coach->facebook_url)
                                     <a href="{{ $coach->facebook_url }}"><i class="fab fa-facebook-f"></i></a>
-                                    @if ($coach->twitter_url)
-                                        <a href="{{ $coach->twitter_url }}">
-                                            <svg enable-background="new 0 0 1226.37 1226.37"
-                                                viewBox="0 0 1226.37 1226.37" xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="m727.348 519.284 446.727-519.284h-105.86l-387.893 450.887-309.809-450.887h-357.328l468.492 681.821-468.492 544.549h105.866l409.625-476.152 327.181 476.152h357.328l-485.863-707.086zm-144.998 168.544-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721h-162.604l-323.311-462.446z">
-                                                </path>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                                <g></g>
-                                            </svg>
-                                        </a>
-                                    @endif
-                                    @if ($coach->instagram_url)
-                                        <a href="{{ $coach->instagram_url }}"><i class="fab fa-instagram"></i></a>
-                                    @endif
+                                @endif
+                                @if ($coach->twitter_url)
+                                    <a href="{{ $coach->twitter_url }}">
+                                        <svg enable-background="new 0 0 1226.37 1226.37" viewBox="0 0 1226.37 1226.37"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="m727.348 519.284 446.727-519.284h-105.86l-387.893 450.887-309.809-450.887h-357.328l468.492 681.821-468.492 544.549h105.866l409.625-476.152 327.181 476.152h357.328l-485.863-707.086zm-144.998 168.544-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721h-162.604l-323.311-462.446z">
+                                            </path>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                            <g></g>
+                                        </svg>
+                                    </a>
+                                @endif
+                                @if ($coach->instagram_url)
+                                    <a href="{{ $coach->instagram_url }}"><i class="fab fa-instagram"></i></a>
+                                @endif
                             </div>
                         </div>
                     </div>
