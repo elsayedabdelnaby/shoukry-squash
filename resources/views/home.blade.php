74d81a68ea7e55199a4a4d7ac2c484a63dbb3b0a
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2024-09-21 06:21:56 +0300

    remove slogan from slider, home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,6 @@
                 <div class="container">
                     <div class="slide-content" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="50">
                         <span class="sub-title"> Shoukry squash academy </span>
-                        <h1>Elevate Your Game To A New Dimension</h1>
                     </div>
                 </div>
             </div>
